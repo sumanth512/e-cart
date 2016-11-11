5aa052147ed07996c01f86d87422aec59af1f529
sumanth reddy <sumanthreddy512@gmail.com>
2016-11-11 12:36:50 +0530

    Revert "first"
    This reverts commit 0bc74d6f21c8411fb0c6035d684b52044e9d0136.

--- js/controllers/carViewtCtrl.js
+++
@@ -1,6 +0,0 @@
-var app=angular.module("myApp")
-app.controller("cartCtrl",function ($scope,cartSvc) {
-
-        $scope.myCart=cartSvc.getItems();
-console.log($scope.myCart)
-})
--- js/services/productViewSvc.js
+++
@@ -1,8 +0,0 @@
-var app=angular.module("myApp")
-app.service("productViewSvc",function($http){
-
-    this.getProductsDetails=function(){
-        return $http.get("http://localhost/cafeapi/products.php")
-    }
-
-})
